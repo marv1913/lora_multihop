ef806232d35cec18bb7929e6e0eb63b621988e3c
Marvin Rausch <marvin.rausch@live.de>
2021-05-21 20:49:02 +0200

    changed file structure

--- integration_test/start_integration_test_container.sh
+++ integration_test/start_integration_test_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build . -t multihop-peer
-docker run --network host --env-file env_node_1 --name node-1 -d multihop-peer:latest
-docker run --network host --env-file env_node_2 --name node-2 -d multihop-peer:latest
+docker run --network host --env-file integration_test/env_node_1 --name node-1 -d multihop-peer:latest
+docker run --network host --env-file integration_test/env_node_2 --name node-2 -d multihop-peer:latest
