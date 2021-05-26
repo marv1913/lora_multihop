6000433cd544141919da78edf4508653e0f802fa
Marvin Rausch <marvin.rausch@live.de>
2021-05-26 12:22:18 +0200

    wrote integration tests for multi hop communication

--- integration_test/two_nodes/start_integration_test_container.sh
+++ integration_test/two_nodes/start_integration_test_container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build . -t multihop-peer
-docker run --network host --env-file integration_test/env_node_1 --name node-1 -d multihop-peer:latest
+docker run --network host --env-file integration_test/two_nodes/env_node_1 --name node-1 -d multihop-peer:latest
 sleep 5
-docker run --network host --env-file integration_test/env_node_2 --name node-2 -d multihop-peer:latest
+docker run --network host --env-file integration_test/two_nodes/env_node_2 --name node-2 -d multihop-peer:latest
